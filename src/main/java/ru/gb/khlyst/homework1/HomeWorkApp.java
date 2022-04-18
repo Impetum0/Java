0dc528c8d43325f7b73f15fa9ff9f93123421b29
Impetum0 <khlyst01@mail.ru>
2022-04-18 16:42:38 +0300

    Task 3

--- src/main/java/ru/gb/khlyst/homework1/HomeWorkApp.java
+++ src/main/java/ru/gb/khlyst/homework1/HomeWorkApp.java
@@ -1,8 +1,11 @@
 package ru.gb.khlyst.homework1;
 
+import java.util.Scanner;
+
 public class HomeWorkApp {
     public static void main(String[] args) {
         printThreeWords();
+        checkSumSign();
     }
 
     public static void printThreeWords() {
@@ -10,4 +13,21 @@ public class HomeWorkApp {
         System.out.println("Banana");
         System.out.println("Apple");
     }
-}
+
+
+    public static void checkSumSign() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Print a");
+        int a = scanner.nextInt();
+        System.out.println("Print b");
+        int b = scanner.nextInt();
+        int result = a + b;
+            if (result >= 0){
+                System.out.println("Сумма положительная");
+            }
+            else {
+                System.out.println("Сумма отрицательная");
+            }
+        }
+    }
+
