ef71a86e3a04b4b922e8ece37bae8125642f1eb8
Impetum0 <khlyst01@mail.ru>
2022-04-21 17:15:51 +0300

    task 2

--- src/main/java/ru/gb/khlyst/homework2/Homework2.java
+++ src/main/java/ru/gb/khlyst/homework2/Homework2.java
@@ -1,11 +1,26 @@
 package ru.gb.khlyst.homework2;
 
+import java.util.Scanner;
+
 public class Homework2 {
     public static void main(String[] args) {
         System.out.println(Task1(6, 5));
+        Task2();
     }
 
     public static boolean Task1(int a, int b) {
         return a + b <= 20 && a + b >= 10;
+    }
+
+    public static void Task2(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Print a");
+        int a = scanner.nextInt();
+        if (a >= 0){
+            System.out.println("positive");
+        }
+        else {
+            System.out.println("negative");
+        }
     }
 }
